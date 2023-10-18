f9f50a347d7c1818996abc893430f7e8baa81913
Chlyeonseo <dustj0719@gmail.com>
2023-10-18 12:04:25 +0900

    리액트 스타트 올리기3

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -51,3 +51,18 @@ const memberReducer = (state = initMember, action) => {
 const reducers = combineReducers({ memberReducer })
 
 export default reducers
+
+/*
+라이브러리
+
+useReducer는 리액트의 내장된 hook 중 하나로, 어플리케이션 상태관리를 위한 방법입니다
+-hook : 리액트에서 사용하는 기능
+반면 Redux는 별도의 라이브러리로 리액트와 독립적으로 사용이 가능하며
+어플리케이션의 전역상태관리를 위한 고급 도구입니다
+
+범위
+useReducer는 일반적으로 컴포넌트 내부에서 상태를 관리합니다
+한 컴포넌트 내에서만 상태를 공유하고 업데이트하는 등 지역적입니다
+
+반면에 Redux는 여러 컴포넌트에서 전역상태를 공유하고 업데이트 하는 등 전역적입니다
+*/
